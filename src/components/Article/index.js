bf17377f404cc0c4d321129fe9d6070f94a73da0
joyci <xxjoycixx@gmail.com>
2023-01-08 14:58:48 -0300

    styling project

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -5,9 +5,15 @@ const Article = () =>{
         <section className="container">
             <h3>Articles</h3>
             <div className="mt-3 container-posts">
-                <Post />
-                <Post />
-                <Post />
+                <Post subtitle="tecnologia" title="O guia definitivo sobre o blog">
+                    1Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper lorem vitae maximus volutpat. Sed nibh diam, porta a finibus vitae, laoreet consectetur lectus. Suspendisse et odio vitae turpis aliquet tincidunt.
+                </Post>
+                <Post subtitle="fotografia" title="Quais as melhores câmeras em 2021">
+                    2Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper lorem vitae maximus volutpat. Sed nibh diam, porta a finibus vitae, laoreet consectetur lectus. Suspendisse et odio vitae turpis aliquet tincidunt.
+                </Post>
+                <Post subtitle="cinema" title="Os 10 filmes com maior bilheteria">
+                    3Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper lorem vitae maximus volutpat. Sed nibh diam, porta a finibus vitae, laoreet consectetur lectus. Suspendisse et odio vitae turpis aliquet tincidunt.
+                </Post>
             </div>
         </section>
     )
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,10 +1,10 @@
-const Post = () =>{
+const Post = (props) =>{
     return(
         <div className="post mx-2">
             <div className="img-post"></div>
-            <h5>Tecnologia</h5>
-            <h3>O guia definitivo do block</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut semper lorem vitae maximus volutpat. Sed nibh diam, porta a finibus vitae, laoreet consectetur lectus. Suspendisse et odio vitae turpis aliquet tincidunt.</p>
+            <h5>{props.subtitle}</h5>
+            <h3>{props.title}</h3>
+            <p className="mt-1">{props.children}</p>
             <div className="flex pt-2">
                 <div className="img-profile"></div>
                 <div className="desc-profile ml-2">
